b4981e4a12672e2bca8aa824f0561ab7427a892c
John Zhang <118221197+john-zhang-dev@users.noreply.github.com>
2025-03-21 20:51:23 +1300

    perf: support query filters for transacations, invoices and payments (#5)

--- src/Tools/Accounting/Invoices.ts
+++ src/Tools/Accounting/Invoices.ts
@@ -6,13 +6,28 @@ export const ListInvoicesTool: IMcpServerTool = {
   requestSchema: {
     name: "list_invoices",
     description: "Retrieves sales invoices or purchase bills",
-    inputSchema: { type: "object", properties: {} },
+    inputSchema: {
+      type: "object",
+      properties: {
+        where: {
+          type: "string",
+          description: "Filter invoices. See example",
+          example:
+            "Date >= DateTime(2015, 01, 01) && Date < DateTime(2015, 12, 31), DueDate < DateTime(2015, 12, 31)",
+        },
+      },
+    },
     output: { content: [{ type: "text", text: z.string() }] },
   },
-  requestHandler: async () => {
+  requestHandler: async (request) => {
+    const whereClause = request.params.arguments
+      ? (request.params.arguments.where as string)
+      : undefined;
     const response =
       await XeroClientSession.xeroClient.accountingApi.getInvoices(
-        XeroClientSession.activeTenantId()!!
+        XeroClientSession.activeTenantId()!!,
+        undefined,
+        whereClause
       );
     const invoices = response.body.invoices || [];
     return {
